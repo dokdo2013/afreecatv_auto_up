8ca8aafde9bed1777cb9ebcdcfe257a1961d5eca
johyeonwoo <jwcho@jobmap.kr>
2020-11-09 20:04:18 +0900

    ver 1.0.3

--- inject.js
+++ inject.js
@@ -1,4 +1,9 @@
-$("#player_area > div.htmlplayer_wrap > div > div.player_item_list.on.pip > ul > li.up_recommend > button").trigger("click");
+setTimeout(() => {
+    // 접속 3초 후 추천
+    $("#player_area > div.htmlplayer_wrap > div > div.player_item_list.on.pip > ul > li.up_recommend > button").trigger("click");
+}, 3000);
+
 setInterval(function(){
+    // 매 61초마다 꿀잼각 누르기
     $("#player_area > div.htmlplayer_wrap > div > div.player_item_list.on.pip > ul > li.honey_fun > button").trigger("click");
 }, 61000);
