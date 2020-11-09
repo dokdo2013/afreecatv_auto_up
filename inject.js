b39ebe23c413c9af3c71183eb99d0f612c7c33d4
johyeonwoo <jwcho@jobmap.kr>
2020-11-09 20:02:17 +0900

    ver 1.0.2

--- inject.js
+++ inject.js
@@ -1,3 +1,4 @@
+$("#player_area > div.htmlplayer_wrap > div > div.player_item_list.on.pip > ul > li.up_recommend > button").trigger("click");
 setInterval(function(){
     $("#player_area > div.htmlplayer_wrap > div > div.player_item_list.on.pip > ul > li.honey_fun > button").trigger("click");
 }, 61000);
